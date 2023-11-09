d3e4231166705a583e1d8383481b947492ad05f4
Anton Lage <anton.lage@d-i-s.dk>
2023-11-09 11:21:15 +0000

    Created an install script, for install extensions

---
+++ .vscode/installExtensions.sh
@@ -0,0 +1,7 @@
+
+code --install-extension platformio.platformio-ide
+code --install-extension thecreativedodo.usbip-connect 
+code --install-extension ms-vscode.cpptools
+code --install-extension mhutchie.git-graph
+
+
