67916c689fbca04756ad79096e3a7f518ddcdbdb
AnthonyDTU <71980488+AnthonyDTU@users.noreply.github.com>
2023-11-09 12:23:25 +0100

    Update installExtensions.sh

--- .vscode/installExtensions.sh
+++ .vscode/installExtensions.sh
@@ -3,5 +3,6 @@ code --install-extension platformio.platformio-ide
 code --install-extension thecreativedodo.usbip-connect 
 code --install-extension ms-vscode.cpptools
 code --install-extension mhutchie.git-graph
+code --install-extension qhoekman.language-plantuml
 
 
