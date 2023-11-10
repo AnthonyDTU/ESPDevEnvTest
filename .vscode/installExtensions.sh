4b6cfd1e36a0cfea5dad22c169fb226a9000dc22
AnthonyDTU <71980488+AnthonyDTU@users.noreply.github.com>
2023-11-10 08:56:08 +0100

    Update installExtensions.sh

--- .vscode/installExtensions.sh
+++ .vscode/installExtensions.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 code --install-extension platformio.platformio-ide
 code --install-extension thecreativedodo.usbip-connect 
 code --install-extension ms-vscode.cpptools
@@ -6,3 +5,5 @@ code --install-extension mhutchie.git-graph
 code --install-extension qhoekman.language-plantuml
 
 
+# from https://code.visualstudio.com/remote/advancedcontainers/start-processes
+exec "$@"
